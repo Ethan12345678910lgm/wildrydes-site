7b6e5b38c93046b8d57cf7154ce1d1b3eb1e0365
Ethan12345678910lgm <59320579+Ethan12345678910lgm@users.noreply.github.com>
2025-06-18 11:58:44 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_wxyW0qNQW', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6u1qcedrvb5ee8cau38d2ot7qf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_iw311iZQA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5ilol6h8kvc02aumjla486931t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
